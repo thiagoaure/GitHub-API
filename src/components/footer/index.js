20e3403597ec3b68173503adb78ac0488ad3cade
thiago aureliano <thiagoh544@gmail.com>
2022-03-03 22:42:29 -0300

    yarn.lock Provider e Icon feat: add Provider e Icone no Footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import useGithub from "../../hooks/github-hooks";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
   const { githubState } = useGithub();
 
   return (
     <footer>
-        <p>Footer</p>
+    {(!!githubState.user.name) ? (
+        <>
+            <FontAwesomeIcon icon="fa-brands fa-github-square" />
+            <p>&copy;{githubState.user.name}</p>
+        </>
+    ) : (
+        <>
+            <FontAwesomeIcon icon="fa-brands fa-github-square" />
+            <p>&copy; Sem Usuário Buscado</p>
+        </>
+    )}
     </footer>
   );
 };
