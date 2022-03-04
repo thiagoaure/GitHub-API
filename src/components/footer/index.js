9c11e6630eafdb9144126db476f673bba449e650
thiago aureliano <thiagoh544@gmail.com>
2022-03-03 23:43:00 -0300

    Estilizacao e Data feat: estilizacao e data do footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,23 +1,32 @@
 import React from "react";
 import useGithub from "../../hooks/github-hooks";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Wrapper} from '../footer/styled';
 
 const Footer = () => {
   const { githubState } = useGithub();
+  const date = new Date();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
 
   return (
     <footer>
-    {(!!githubState.user.name) ? (
-        <>
-            <FontAwesomeIcon icon="fa-brands fa-github-square" />
-            <p>&copy;{githubState.user.name}</p>
-        </>
-    ) : (
-        <>
-            <FontAwesomeIcon icon="fa-brands fa-github-square" />
-            <p>&copy; Sem Usuário Buscado</p>
-        </>
-    )}
+        <Wrapper>
+            {(!!githubState.user.name) ? (
+                <>
+                    <FontAwesomeIcon icon="fa-brands fa-github-square" />
+                    <p>&copy;{githubState.user.name}</p>
+                    <p>{`${day}/${month+1}/${year}`}</p>
+                </>
+            ) : (
+                <>
+                    <FontAwesomeIcon icon="fa-brands fa-github-square" />
+                    <p>&copy; Sem Usuário Buscado</p>
+                    <p>{`${day}/${month+1}/${year}`}</p>
+                </>
+            )}
+        </Wrapper>
     </footer>
   );
 };
---
+++ src/components/footer/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    text-align: center;
+    padding: 5px;
+`;
+
